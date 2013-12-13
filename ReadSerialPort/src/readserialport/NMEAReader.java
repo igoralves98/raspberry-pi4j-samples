ba7a265c76d11ebe28c193b2f2c2b0a2a4664632
unknown <olediour@OLEDIOUR-LAP.oradev.oraclecorp.com>
2013-12-13 11:46:18 -0800

    Initial checkin

---
+++ ReadSerialPort/src/readserialport/NMEAReader.java
@@ -0,0 +1,97 @@
+package readserialport;
+
+import com.pi4j.io.serial.Serial;
+import com.pi4j.io.serial.SerialDataEvent;
+import com.pi4j.io.serial.SerialDataListener;
+import com.pi4j.io.serial.SerialFactory;
+
+import com.pi4j.io.serial.SerialPortException;
+
+import java.util.Date;
+
+public class NMEAReader
+{
+  public static void main(String args[])
+    throws InterruptedException
+  {
+    int br = 4800;
+    if (args.length > 0)
+    {
+      try
+      {
+        br = Integer.parseInt(args[0]);
+      }
+      catch (Exception ex)
+      {
+        System.err.println(ex.getMessage());
+      }
+    }
+    
+    System.out.println("Serial Communication.");
+    System.out.println(" ... connect using settings: " + Integer.toString(br) +  ", N, 8, 1.");
+    System.out.println(" ... data received on serial port should be displayed below.");
+
+    // create an instance of the serial communications class
+    final Serial serial = SerialFactory.createInstance();
+
+    // create and register the serial data listener
+    serial.addListener(new SerialDataListener()
+    {
+      @Override
+      public void dataReceived(SerialDataEvent event)
+      {
+        // print out the data received to the console
+        System.out.print("Read:" + event.getData());
+      }
+    });
+
+    try
+    {
+      // open the default serial port provided on the GPIO header
+      System.out.println("Opening port [" + Serial.DEFAULT_COM_PORT + "]");
+      serial.open(Serial.DEFAULT_COM_PORT, br);
+
+      // continuous loop to keep the program running until the user terminates the program
+      while (true)
+      {
+        if (serial.isOpen())
+        {
+          try
+          {
+            // write a formatted string to the serial transmit buffer
+            serial.write("CURRENT TIME: %s", new Date().toString());
+  
+            // write a individual bytes to the serial transmit buffer
+            serial.write((byte) 13);
+            serial.write((byte) 10);
+  
+            // write a simple string to the serial transmit buffer
+            serial.write("Second Line");
+  
+            // write a individual characters to the serial transmit buffer
+            serial.write('\r');
+            serial.write('\n');
+  
+            // write a string terminating with CR+LF to the serial transmit buffer
+            serial.writeln("Third Line");
+          }
+          catch (IllegalStateException ex)
+          {
+            ex.printStackTrace();
+          }
+        }
+        else
+        {
+          System.out.println("Not open yet...");
+        }
+        // wait 1 second before continuing
+        Thread.sleep(1000);
+      }
+    }
+    catch (SerialPortException ex)
+    {
+      System.out.println(" ==>> SERIAL SETUP FAILED : " + ex.getMessage());
+      return;
+    }
+  }
+}
