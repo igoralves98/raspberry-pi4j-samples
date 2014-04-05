029c5b7555ec20d933c1ea2c3524be017277703f
Olivier LeDiouris <olivier.lediouris@oracle.com>
2014-04-04 17:01:43 -0700

    Added the HTTP Server/RelayManager

---
+++ Relay.by.http/web/relay.js
@@ -0,0 +1,15 @@
+var set = function(device, state)
+{
+  request(device, state);
+};
+
+var SERVICE_ROOT = "/relay-access";
+
+var request = function(pin, status) // REST Client
+{
+  var restRequest = SERVICE_ROOT + "?dev=" + pin + "&status=" + status;
+  var ajax = new XMLHttpRequest();
+  ajax.open("GET", restRequest, false);
+  ajax.send(null);
+
+};
