b3d274e84ce765a618559d9100a74de703f7c897
Olivier LeDiouris <olivier.lediouris@oracle.com>
2014-05-12 09:22:30 -0700

    VCNL4000 with 7-seg display

---
+++ AdafruitI2C/src/adafruiti2c/sensor/main/VCNL4000ProximityWithDisplay.java
@@ -0,0 +1,83 @@
+package adafruiti2c.sensor.main;
+
+import adafruiti2c.sensor.AdafruitVCNL4000;
+
+import com.pi4j.system.SystemInfo;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+import sevensegdisplay.SevenSegment;
+
+public class VCNL4000ProximityWithDisplay
+{
+  private static boolean go = true;
+  private final static int MIN_AMBIENT =    0;
+  private final static int MAX_AMBIENT = 5500;
+  
+  private static AdafruitVCNL4000 sensor;
+  private static SevenSegment display;
+  
+  public static void main(String[] args) throws Exception
+  {
+    sensor = new AdafruitVCNL4000();
+    display = new SevenSegment(0x70, true);
+
+    int prox    = 0;
+    int ambient = 0;
+    
+    Runtime.getRuntime().addShutdownHook(new Thread()
+                                         {
+                                           public void run()
+                                           {
+                                             go = false;
+                                             try { display.clear(); } catch (IOException ioe) { ioe.printStackTrace(); }
+                                             System.out.println("\nBye");
+                                           }
+                                         });
+    while (go) //  && i++ < 5)
+    {
+      try 
+      { 
+    //      prox = sensor.readProximity();
+        int[] data = sensor.readAmbientProximity();
+        prox    = data[AdafruitVCNL4000.PROXIMITY_INDEX];
+        ambient = data[AdafruitVCNL4000.AMBIENT_INDEX];
+      } 
+      catch (Exception ex) 
+      { 
+        System.err.println(ex.getMessage()); 
+        ex.printStackTrace();
+      }
+//    System.out.println("Ambient:" + ambient + ", Proximity: " + prox); //  + " unit?");
+      int amb = /* 100 - */ Math.min((int)Math.round(100f * ((float)ambient / (float)(MAX_AMBIENT - MIN_AMBIENT))), 100); 
+      System.out.println("Ambient:" + ambient + ", Proximity: " + prox + ", " + amb);
+      // Notice the digit index: 0, 1, 3, 4. 2 is the column ":"
+      int one   = amb / 1000;
+      int two   = (amb - (one * 1000)) / 100;
+      int three = (amb - (one * 1000) - (two * 100)) / 10;
+      int four  = amb % 10;
+      
+//    System.out.println("  --> " + proxPercent + " : " + one + " " + two + "." + three + " " + four);
+      
+      if (one > 0)
+        display.writeDigit(0, one);
+      else
+        display.writeDigitRaw(0, " ");
+      if (two > 0 || one > 0)
+        display.writeDigit(1, two);
+      else
+        display.writeDigitRaw(1, " ");
+      if (one > 0 || two > 0 || three > 0)
+        display.writeDigit(3, three);
+      else
+        display.writeDigitRaw(3, " ");
+      display.writeDigit(4, four);
+      try { Thread.sleep(100L); } catch (InterruptedException ex) { System.err.println(ex.toString()); }
+    }
+  }
+}
