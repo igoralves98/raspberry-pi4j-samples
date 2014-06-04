235dd0fe69033dd32493548f4f9a9830ff5f379b
Olivier LeDiouris <olivier.lediouris@oracle.com>
2014-06-04 16:14:39 -0700

    Endianness...

--- AdafruitI2C/src/adafruiti2c/sensor/AdafruitBMP180.java
+++ AdafruitI2C/src/adafruiti2c/sensor/AdafruitBMP180.java
@@ -18,7 +18,7 @@ public class AdafruitBMP180
 {
   public final static int LITTLE_ENDIAN = 0;
   public final static int BIG_ENDIAN    = 1;
-  private final static int BMP180_ENDIANNESS = LITTLE_ENDIAN;
+  private final static int BMP180_ENDIANNESS = BIG_ENDIAN;
   /*
   Prompt> sudo i2cdetect -y 1
        0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
